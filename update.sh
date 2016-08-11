3e19a06b8e13eeb1f85f6e1d13ba8e4d15e865a0
jofemodo <fernando@zynthian.org>
2016-08-11 18:12:50 +0200

    + Improve install.sh + New update.sh script

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #******************************************************************************
-# MOTOSPIRIT PROJECT: Motospirit Display Install Script
+# MOTOSPIRIT PROJECT: Motospirit Display Update Script
 # 
-# Configure and Install all Software needed by MotoSpirit Display
+# Update MotoSpirit Display software
 # 
 # Copyright (C) 2016 Fernando Moyano <jofemodo@zynthian.org>
 #
@@ -24,46 +24,21 @@
 
 MSDIR=~/motospirit
 
-# Requirements ...
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install python3-pyqt4 pyqt4-dev-tools libcanberra-gtk0 mercurial automake m4 can-utils joe tree fbi
-sudo apt-get install i2c-tools libi2c-dev
-
-# Install python-can
-cd $MSDIR/src
-hg clone https://bitbucket.org/hardbyte/python-can
-cd python-can
-sudo python3 setup.py install
-
-# Install libcanopen
-cd $MSDIR/src
-git clone https://github.com/rscada/libcanopen.git
-cd libcanopen
-rm -f ./depcomp
-ln -s /usr/share/automake-1.14/depcomp .
-./configure
-make
-sudo make install
-cd python
-sudo python3 ./setup.py install
-sudo ldconfig
+cd $MSDIR
+git pull
 
 # Install fonts
 sudo cp $MSDIR/src/fonts/* /usr/local/share/fonts
 sudo fc-cache -fv
-
-# Add user pi to video group
-sudo useradd -G video pi
 
 # Copy xinitrc
 cp -f $MSDIR/sbin/xinitrc ~/.xinitrc
 cp -f $MSDIR/sbin/xinitrc ~/.xsession
 
 # Splash Screen
-sudo cp -f $MSDIR/config/asplashscreen /etc/init.d/asplashscreen 
-sudo ln -s /etc/init.d/asplashscreen /etc/rcS.d/S01asplashscreen
-sudo systemctl enable asplashscreen
+#sudo cp -f $MSDIR/config/asplashscreen /etc/init.d/asplashscreen 
+#sudo ln -s /etc/init.d/asplashscreen /etc/rcS.d/S01asplashscreen
+#sudo systemctl enable asplashscreen
 
 # Make
 cd $MSDIR/src
