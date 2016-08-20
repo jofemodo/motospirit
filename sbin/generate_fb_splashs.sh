01950a84bd9ebd5a3268afa76bc83678533e4745
jofemodo <fernando@zynthian.org>
2016-08-20 12:55:40 +0200

    + Update splash images to real size 800x480

--- sbin/generate_fb_splashs.sh
+++ sbin/generate_fb_splashs.sh
@@ -2,8 +2,8 @@
 
 MSDIR=~/motospirit
 
-sudo fbi --noverbose -vt 2 -d /dev/fb0 $MSDIR/src/img/logo_motospirit_800x450.png
+sudo fbi --noverbose -vt 2 -d /dev/fb0 $MSDIR/src/img/logo_motospirit_800x480.png
 sudo cat /dev/fb0 > $MSDIR/src/img/fb_motospirit.raw
 
-sudo fbi --noverbose -vt 2 -d /dev/fb0 $MSDIR/src/img/logo_motospirit_error_800x450.png
+sudo fbi --noverbose -vt 2 -d /dev/fb0 $MSDIR/src/img/logo_motospirit_error_800x480.png
 sudo cat /dev/fb0 > $MSDIR/src/img/fb_motospirit_error.raw
